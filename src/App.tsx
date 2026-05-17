b634e7086ec3a864cf020ea6699af8b14305c6a6
Tanziruz <Tanziruz25@gmail.com>
2026-05-17 11:52:35 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,79 @@
+import { Canvas, useFrame, type ThreeElement } from "@react-three/fiber"
+import { CameraControls, OrbitControls, Stars, useHelper } from "@react-three/drei"
+import { DirectionalLightHelper, DirectionalLight } from "three"
+import { useRef } from "react"
+import { Bloom, EffectComposer } from "@react-three/postprocessing"
+import * as THREE from "three"
+
+function DirectionalLightWithHelper() {
+  const light = useRef<DirectionalLight | null>(null)
+  useHelper(light, DirectionalLightHelper, 1, 'red')
+  return (
+    <directionalLight
+      // castShadow
+      intensity={5}
+      ref={light}
+      position={[5, 5, 5]}
+      color="white"
+    />
+  )
+}
+function RotatingMesh() {
+  const waveRotation = useRef<THREE.Mesh | null>(null)
+  useFrame(() => {
+    if (waveRotation.current) {
+      waveRotation.current.rotation.y += 10;
+    }
+  })
+    return (
+       <group rotation={[0,0,-30.4 * Math.PI / 180]}>
+         <mesh>
+               <sphereGeometry args={[1, 64, 64]} />
+            
+               <meshBasicMaterial color="#509bdd" />
+             </mesh>
+          <mesh ref={waveRotation} rotation={[0,0, -3.4 * Math.PI/180]}>
+            <cylinderGeometry args={[0.1, 0.1, 1000, 32]} />
+            
+            <meshStandardMaterial color="#ffffff" />
+          </mesh>
+ <pointLight intensity={12} distance={10} color="#88ccff" />
+
+       </group>
+    )
+}
+
+const App = () => {
+ 
+
+  return (
+    <div className="w-screen h-dvh">  
+      <Canvas className="bg-black">
+
+        <CameraControls />
+        {/* <OrbitControls /> */}
+  <ambientLight />
+  <EffectComposer>
+    <Bloom
+      intensity={12} // The bloom intensity.
+      kernelSize={3} // The bloom kernel size.
+      luminanceThreshold={0.05} // The luminance threshold. Lower to include more bright areas.
+      luminanceSmoothing={0.025} // Smoothness of the luminance threshold. Range is [0, 1].
+    />
+  </EffectComposer>
+  {/* <DirectionalLightWithHelper /> */}
+  <RotatingMesh />
+
+      
+<Stars
+  radius={100}
+  depth={50}
+  count={1000}
+  factor={4}
+/>
+</Canvas>
+    </div>
+  )
+}
+
+export default App
